27c926383d3901f7fc7bd15797ae84b9e10fad7b
Abdul Hadi <abdulhadi2003m@gmail.com>
2023-05-27 13:22:55 +0400

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let items = document.querySelectorAll('.carousel .cards');
+/*let items = document.querySelectorAll('.carousel .cards');
 let next = document.getElementById('next');
 let previous = document.getElementById('previous');
 
@@ -12,3 +12,30 @@ function carouselfun() {
     }
 }
 carouselfun();
+*/
+
+
+let card_index = 1;
+showcards(card_index);
+
+function nextSlide(n) {
+    showcards(card_index += n);
+}
+
+function activeSlide(n) {
+    showcards(card_index = n);
+}
+
+function displaySlides(n) {
+    let i;
+    let cards = document.getElementsByClassName("cards");
+}
+
+if (n > cards.length) { card_index = 1 }
+if (n < 1) { card_index = cards.length }
+for (i = 0; i < cards.length; i++) {
+    cards[i].style.display = "none";
+}
+
+cards[card_index - 1].style.display = "block";
+
